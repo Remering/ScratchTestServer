b4b6206f2875ac64182938feb3711957487768ca
Remering <1015488424@qq.com>
2020-05-18 10:41:00 +0800

    merge all apis

--- src/main/kotlin/com/github/io/remering/starter/api/Api.kt
+++ src/main/kotlin/com/github/io/remering/starter/api/Api.kt
@@ -1,23 +1,37 @@
 package com.github.io.remering.starter.api
 
-import com.github.io.remering.starter.api.account.*
+import com.github.io.remering.starter.api.account.mountAccount
+import com.github.io.remering.starter.api.course.mountCourse
 import com.github.io.remering.starter.api.file.mountGetFile
-import com.github.io.remering.starter.api.file.mountUploadFile
-import com.github.io.remering.starter.api.profile.mountGetProfile
 import com.github.io.remering.starter.api.profile.mountProfile
-import com.github.io.remering.starter.api.profile.mountUpdateProfile
+import com.github.io.remering.starter.api.question.mountQuestion
 import io.vertx.reactivex.core.Vertx
 import io.vertx.reactivex.ext.web.Router
+import io.vertx.reactivex.ext.web.handler.StaticHandler
 
 fun Router.mountApi(vertx: Vertx) {
-  val router = Router.router(vertx)
-  mountSubRouter("/plarform/user", router)
-  with(router) {
+  val userRouter = Router.router(vertx)
+  mountSubRouter("/plarform/user", userRouter)
+  with(userRouter) {
     mountAccount()
     mountProfile()
-    mountUploadFile()
+//    mountUploadFile()
   }
+
+  val teacherRouter = Router.router(vertx)
+  mountSubRouter("/plarform/teacher", teacherRouter)
+  teacherRouter.mountCourse()
+
   mountGetFile()
+
+  val questionRouter = Router.router(vertx)
+  mountSubRouter("/plarform/question", questionRouter)
+  questionRouter.mountQuestion()
+
+  val staticRouter = Router.router(vertx)
+  mountSubRouter("/static", staticRouter)
+
+  staticRouter.route().handler(StaticHandler.create("static"))
 
 }
 
