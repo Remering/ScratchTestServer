cb04b64a60f4aa17c57849b632b83d7d382b40b3
Remering <1015488424@qq.com>
2020-05-18 10:42:35 +0800

    change uuid and teacher from UUID to String

--- src/main/kotlin/com/github/io/remering/starter/table/CourseTable.kt
+++ src/main/kotlin/com/github/io/remering/starter/table/CourseTable.kt
@@ -3,31 +3,24 @@ package com.github.io.remering.starter.table
 import me.liuwj.ktorm.entity.Entity
 import me.liuwj.ktorm.schema.Table
 import me.liuwj.ktorm.schema.varchar
-import java.util.*
 
 interface CourseEntity : Entity<CourseEntity> {
   companion object: Entity.Factory<CourseEntity>()
-  var uuid: UUID
+  var uuid: String
   var name: String
   var introduction: String
   var picture: String
-  var teacher: UUID
+  var teacher: String
   var video: String
   var file: String
 }
 
 object Courses: Table<CourseEntity>("courses") {
-  val uuid by varchar("uuid")
-    .transform(UUID::fromString, UUID::toString)
-    .primaryKey()
-    .bindTo { it.uuid }
+  val uuid by varchar("uuid").primaryKey().bindTo { it.uuid }
   val name by varchar("name").bindTo { it.name }
   val introduction by varchar("introduction").bindTo { it.introduction }
   val picture by varchar("picture").bindTo { it.picture }
-  val teacher by varchar("teacher")
-    .transform(UUID::fromString, UUID::toString)
-    .primaryKey()
-    .bindTo { it.teacher }
+  val teacher by varchar("teacher").bindTo { it.teacher }
   val video by varchar("video").bindTo { it.video }
   val file by varchar("file").bindTo { it.file }
 }
