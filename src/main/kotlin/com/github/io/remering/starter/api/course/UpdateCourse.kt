4568ff66002a22987a0b87299d9df799f7f84566
Remering <1015488424@qq.com>
2020-05-18 10:23:54 +0800

    reformat code

--- src/main/kotlin/com/github/io/remering/starter/api/course/UpdateCourse.kt
+++ src/main/kotlin/com/github/io/remering/starter/api/course/UpdateCourse.kt
@@ -1,24 +1,19 @@
 package com.github.io.remering.starter.api.course
 
 import com.github.io.remering.starter.*
+import com.github.io.remering.starter.api.pojo.Course
 import com.github.io.remering.starter.table.CourseEntity
 import com.github.io.remering.starter.table.Courses
 import io.vertx.core.json.Json
 import io.vertx.kotlin.core.json.get
+import io.vertx.reactivex.ext.web.FileUpload
 import io.vertx.reactivex.ext.web.Router
 import me.liuwj.ktorm.dsl.eq
 import me.liuwj.ktorm.entity.sequenceOf
 import me.liuwj.ktorm.entity.singleOrNull
 import java.util.*
 
-data class UpdateCourseRequestBody(
-  val uuid: String = "",
-  val name: String? = null,
-  val introduction: String? = null,
-  val picture: String? = null,
-  val video: String? = null,
-  val file: String? = null
-)
+
 
 typealias UpdateCourseResponseBody = CreateCourseResponseBody
 
@@ -34,16 +29,14 @@ fun Router.mountUpdateCourse() {
             UpdateCourseResponseBody(
               ERROR, "权限不足"
             )
-          ))
+          )
+        )
         return@handler
       }
-      val requestBody = try {
-        context.bodyAsJson?.mapTo(UpdateCourseRequestBody::class.java)
-      } catch (e: Throwable) {
-        context.fail(e)
-        return@handler
-      }
-      if (requestBody == null) {
+
+
+      val courseUUID = context.request().getParam("uuid")
+      if (courseUUID == null) {
         context.response().end(Json.encode(
           UpdateCourseResponseBody(
             ERROR, "参数错误"
@@ -51,32 +44,58 @@ fun Router.mountUpdateCourse() {
         ))
         return@handler
       }
-      worker.rxExecuteBlocking<CourseEntity> {
-        it.complete(database.sequenceOf(Courses).singleOrNull {
-          it.uuid eq UUID.fromString(requestBody.uuid)
-        })
-      }.doOnError(context::fail)
-      .subscribe { course ->
-        if (course == null) {
-          context.response().end(Json.encode(
+      val name: String? = context.request().getParam("name")
+      val introduction: String? = context.request().getParam("introduction")
+
+
+      val courseEntity = database.sequenceOf(Courses).singleOrNull {
+        it.uuid eq courseUUID
+      }
+
+      if (courseEntity == null) {
+        context.response().end(
+          Json.encode(
             UpdateCourseResponseBody(
               ERROR, "未找到课程"
             )
-          ))
-          return@subscribe
+          )
+        )
+        return@handler
+      }
+
+
+      var fileUpload: FileUpload? = null
+      var pictureUpload: FileUpload? = null
+      var videoUpload: FileUpload? = null
+
+      context.fileUploads().forEach {
+        when(it.name()) {
+          "file" -> fileUpload = it
+          "picture" -> pictureUpload = it
+          "video" -> videoUpload = it
         }
-        course.name = requestBody.name?:course.name
-        course.introduction = requestBody.introduction?:course.introduction
-        course.picture = requestBody.picture?:course.picture
-        course.video = requestBody.video?:course.video
-        course.file = requestBody.file?:course.file
-        course.flushChanges()
-        context.response().end(Json.encode(
+      }
+
+      val fs = context.vertx().fileSystem()
+      val fileUrl = fileUpload?.getUrl(fileUpload!!.renameWithExtension(fs))
+      val pictureUrl = pictureUpload?.getUrl(pictureUpload!!.renameWithExtension(fs))
+      val videoUrl = videoUpload?.getUrl(videoUpload!!.renameWithExtension(fs))
+
+      courseEntity.name = name?: courseEntity.name
+      courseEntity.introduction = introduction?: courseEntity.introduction
+      courseEntity.file = fileUrl?: courseEntity.file
+      courseEntity.picture = pictureUrl?: courseEntity.picture
+      courseEntity.video = videoUrl?: courseEntity.video
+
+      courseEntity.flushChanges()
+
+      context.response().end(
+        Json.encode(
           UpdateCourseResponseBody(
             SUCCESS, "更新成功",
-            Course(course)
+            Course(courseEntity, context.request().host())
           )
-        ))
-      }
-  }
+        )
+      )
+    }
 }
