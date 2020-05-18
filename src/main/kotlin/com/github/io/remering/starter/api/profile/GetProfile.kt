625d424081c6885173469cecbada33124d12ae12
Remering <1015488424@qq.com>
2020-05-18 10:38:23 +0800

    move pojos to pojo/Profiles.kt

--- src/main/kotlin/com/github/io/remering/starter/api/profile/GetProfile.kt
+++ src/main/kotlin/com/github/io/remering/starter/api/profile/GetProfile.kt
@@ -1,6 +1,8 @@
 package com.github.io.remering.starter.api.profile
 
 import com.github.io.remering.starter.SUCCESS
+import com.github.io.remering.starter.api.pojo.GetProfileResponseBody
+import com.github.io.remering.starter.api.pojo.Profile
 import com.github.io.remering.starter.database
 import com.github.io.remering.starter.jwtAuthHandler
 import com.github.io.remering.starter.table.Avatars
@@ -10,46 +12,39 @@ import io.vertx.reactivex.ext.web.Router
 import me.liuwj.ktorm.dsl.eq
 import me.liuwj.ktorm.entity.sequenceOf
 import me.liuwj.ktorm.entity.singleOrNull
-import java.util.*
 
 
-class Profile(
-  val username: String,
-  val uuid: String,
-  val avatarUrl: String? = null,
-  val role: Int
-)
 
-class GetProfileResponseBody(
-  val code: Int,
-  val message: String,
-  val profile: Profile? = null
-)
 
 fun Router.mountGetProfile() {
   get("/getProfile")
     .handler(jwtAuthHandler)
     .handler { context ->
-      val user = context.user()
-      val principal = user.principal()
-      val uuid: String = principal["uuid"]
-      val avatarUrl = database.sequenceOf(Avatars).singleOrNull {
-        it.uuid eq UUID.fromString(uuid)
-      }?.avatarUrl
+      try {
+        val user = context.user()
+        val principal = user.principal()
+        val uuid: String = principal["uuid"]
+        val avatarUrl = database.sequenceOf(Avatars).singleOrNull {
+          it.uuid eq uuid
+        }?.avatarUrl
 
-      context.response().end(
-        Json.encode(
-          GetProfileResponseBody(
-            code = SUCCESS,
-            message = "操作成功",
-            profile = Profile(
-              username = principal["username"],
-              uuid = uuid,
-              avatarUrl = avatarUrl,
-              role = principal["role"]
+        context.response().end(
+          Json.encode(
+            GetProfileResponseBody(
+              code = SUCCESS,
+              message = "操作成功",
+              profile = Profile(
+                username = principal["username"],
+                uuid = uuid,
+                avatarUrl = "http://${context.request().host()}/$avatarUrl",
+                role = principal["role"],
+                email = principal["email"]
+              )
             )
           )
         )
-      )
+      } catch (t: Throwable) {
+        context.fail(t)
+      }
     }
 }
