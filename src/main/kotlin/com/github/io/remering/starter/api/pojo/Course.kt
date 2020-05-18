c93273047096478b3959b2bb4f40ac8944ed7b32
Remering <1015488424@qq.com>
2020-05-18 10:37:28 +0800

    clean up

--- src/main/kotlin/com/github/io/remering/starter/api/pojo/Course.kt
+++ src/main/kotlin/com/github/io/remering/starter/api/pojo/Course.kt
@@ -1,10 +1,6 @@
 package com.github.io.remering.starter.api.pojo
 
-import com.github.io.remering.starter.api.course.mountCreateCourse
-import com.github.io.remering.starter.api.course.mountGetCourses
-import com.github.io.remering.starter.api.course.mountUpdateCourse
 import com.github.io.remering.starter.table.CourseEntity
-import io.vertx.reactivex.ext.web.Router
 
 data class Course (
   val uuid: String = "",
