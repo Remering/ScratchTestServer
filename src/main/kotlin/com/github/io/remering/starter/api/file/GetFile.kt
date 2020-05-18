2a794ea55584a46247048e5890aeef20977bb02f
Remering <1015488424@qq.com>
2020-05-18 10:24:23 +0800

    add get file api

--- src/main/kotlin/com/github/io/remering/starter/api/file/GetFile.kt
+++ src/main/kotlin/com/github/io/remering/starter/api/file/GetFile.kt
@@ -1,15 +1,11 @@
 package com.github.io.remering.starter.api.file
 
 import com.github.io.remering.starter.USER_UPLOAD_FILE_DIRECTORY
-import com.github.io.remering.starter.jwtAuthHandler
 import io.vertx.reactivex.ext.web.Router
 import io.vertx.reactivex.ext.web.handler.StaticHandler
 
 fun Router.mountGetFile() {
   get("/user_upload/*")
-    .handler {
-      println("path: ${it.request().path()}")
-      it.next()
-    }
+
     .handler(StaticHandler.create(USER_UPLOAD_FILE_DIRECTORY))
 }
