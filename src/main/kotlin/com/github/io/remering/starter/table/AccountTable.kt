243f3fdc6e23df5dbb87a6fb10c0e568573a95c4
Remering <1015488424@qq.com>
2020-05-18 10:42:09 +0800

    change uuid from UUID to String

--- src/main/kotlin/com/github/io/remering/starter/table/AccountTable.kt
+++ src/main/kotlin/com/github/io/remering/starter/table/AccountTable.kt
@@ -1,12 +1,14 @@
 package com.github.io.remering.starter.table
 
 import me.liuwj.ktorm.entity.Entity
-import me.liuwj.ktorm.schema.*
-import java.util.*
+import me.liuwj.ktorm.schema.Table
+import me.liuwj.ktorm.schema.int
+import me.liuwj.ktorm.schema.text
 
 interface AccountEntity: Entity<AccountEntity> {
-  companion object: Entity.Factory<AccountEntity>()
-  val uuid: UUID
+  companion object : Entity.Factory<AccountEntity>()
+
+  val uuid: String
   val username: String
   var password: String
   val email: String
@@ -15,10 +17,7 @@ interface AccountEntity: Entity<AccountEntity> {
 }
 
 object Accounts: Table<AccountEntity>("accounts") {
-  val uuid by text("uuid").primaryKey().transform(
-    fromUnderlyingValue = UUID::fromString,
-    toUnderlyingValue = UUID::toString
-  ).bindTo { it.uuid }
+  val uuid by text("uuid").primaryKey().bindTo { it.uuid }
   val username by text("username").bindTo { it.username }
   val password by text("password").bindTo { it.password }
   val email by text("email").bindTo { it.email }
