b1aee112531bc23d91f58d1d87a6bf8603305139
Remering <1015488424@qq.com>
2020-05-18 10:24:09 +0800

    merge all course apis

--- src/main/kotlin/com/github/io/remering/starter/api/course/Course.kt
+++ src/main/kotlin/com/github/io/remering/starter/api/course/Course.kt
@@ -1,23 +1,9 @@
 package com.github.io.remering.starter.api.course
 
-import com.github.io.remering.starter.table.CourseEntity
+import io.vertx.reactivex.ext.web.Router
 
-data class Course (
-  val uuid: String = "",
-  var name: String = "",
-  var introduction: String = "",
-  var picture: String = "",
-  var teacher: String = "",
-  var video: String = "",
-  var file: String = ""
-) {
-  constructor(entity: CourseEntity): this(
-    entity.uuid.toString(),
-    entity.name,
-    entity.introduction,
-    entity.picture,
-    entity.teacher.toString(),
-    entity.video,
-    entity.file
-  )
+fun Router.mountCourse() {
+  mountCreateCourse()
+  mountUpdateCourse()
+  mountGetCourses()
 }
