0710d7d46c8207ed272385eb0ef9b560a0e222a0
Remering <1015488424@qq.com>
2020-05-18 10:39:06 +0800

    reformat code

--- src/main/kotlin/com/github/io/remering/starter/api/profile/UpdateProfile.kt
+++ src/main/kotlin/com/github/io/remering/starter/api/profile/UpdateProfile.kt
@@ -1,58 +1,56 @@
 package com.github.io.remering.starter.api.profile
 
-import com.github.io.remering.starter.*
+import com.github.io.remering.starter.ERROR
+import com.github.io.remering.starter.SUCCESS
+import com.github.io.remering.starter.api.course.getUrl
+import com.github.io.remering.starter.api.course.renameWithExtension
+import com.github.io.remering.starter.api.pojo.Profile
+import com.github.io.remering.starter.api.pojo.UpdateProfileResponseBody
+import com.github.io.remering.starter.database
+import com.github.io.remering.starter.jwtAuthHandler
+import com.github.io.remering.starter.table.AvatarEntity
 import com.github.io.remering.starter.table.Avatars
 import io.vertx.core.json.Json
 import io.vertx.kotlin.core.json.get
 import io.vertx.reactivex.ext.web.Router
 import me.liuwj.ktorm.dsl.eq
-import me.liuwj.ktorm.dsl.insert
+import me.liuwj.ktorm.entity.add
 import me.liuwj.ktorm.entity.sequenceOf
 import me.liuwj.ktorm.entity.singleOrNull
-import java.util.*
 
-data class UpdateProfileRequestBody @JvmOverloads constructor(
-  val avatarUrl: String? = null
-)
-
-class UpdateProfileResponseBody(
-  val code: Int,
-  val message: String,
-  val profile: Profile? = null
-)
 
 fun Router.mountUpdateProfile() {
   post("/updateProfile")
     .handler(jwtAuthHandler)
     .handler { context ->
-      val token = context.request().getHeader(AUTHORIZATION)
-      val requestBody = context.bodyAsJson?.mapTo(UpdateProfileRequestBody::class.java)
-      if (requestBody == null) {
+
+      val user = context.user()
+      val principal = user.principal()
+      val avatarUploads = context.fileUploads().singleOrNull()
+      if (avatarUploads == null) {
         context.response().end(
           Json.encode(
-            UpdateProfileResponseBody(
-              ERROR, "参数错误"
-            )
+            UpdateProfileResponseBody(ERROR, "参数错误")
           )
         )
         return@handler
       }
-      val user = context.user()
-      val (avatarUrl) = requestBody
-      val principal = user.principal()
 
-      if (avatarUrl != null && avatarUrl != principal["avatarUrl"]) {
-        val avatar = database.sequenceOf(Avatars).singleOrNull {
-          it.uuid eq UUID.fromString(principal["uuid"])
+      val avatarUrl = avatarUploads.getUrl(avatarUploads.renameWithExtension(context.vertx().fileSystem()))
+
+      val avatarEntity = database.sequenceOf(Avatars).singleOrNull {
+        it.uuid eq principal.getString("uuid")
+      }
+
+      if (avatarEntity == null) {
+        val newAvatarEntity = AvatarEntity {
+          uuid = principal["uuid"]
+          this.avatarUrl = avatarUrl
         }
-        if (avatar == null) {
-          database.insert(Avatars) {
-            it.uuid to UUID.fromString(principal["uuid"])
-            it.avatarUrl to avatarUrl
-          }
-        } else {
-          avatar.avatarUrl = avatarUrl
-        }
+        database.sequenceOf(Avatars).add(newAvatarEntity)
+      } else {
+        avatarEntity.avatarUrl = avatarUrl
+        avatarEntity.flushChanges()
       }
 
       context.response().end(
@@ -63,8 +61,9 @@ fun Router.mountUpdateProfile() {
             profile = Profile(
               username = principal["username"],
               uuid = principal["uuid"],
-              avatarUrl = avatarUrl ?: principal["avatarUrl"],
-              role = principal["role"]
+              avatarUrl = "http://${context.request().host()}/$avatarUrl",
+              role = principal["role"],
+              email = principal["email"]
             )
           )
         )
